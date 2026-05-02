4f4b880ad4ba263f385a866424d6b14d81982093
Atony <75191600+AtonyGit@users.noreply.github.com>
2026-05-01 20:34:45 -0500

    Reimplement Hide n Seek intro (no music)

--- MiraAPI/GameModes/HideAndSeekMode.cs
+++ MiraAPI/GameModes/HideAndSeekMode.cs
@@ -1,4 +1,12 @@
-﻿using MiraAPI.PluginLoading;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using MiraAPI.GameOptions;
+using MiraAPI.HnsReimplemented.Options;
+using MiraAPI.PluginLoading;
+using MiraAPI.Utilities;
+using PowerTools;
 using UnityEngine;
 
 namespace MiraAPI.GameModes;
@@ -16,4 +24,160 @@ public class HideAndSeekMode : AbstractGameMode
     public override string Description => "You can run, but you can't hide!";
 
     public override Color Color { get; } = new Color32(255, 88, 90, 255);
+
+    public override bool CanReport(DeadBody body) => false;
+    public override bool ShouldShowSabotageMap(MapBehaviour map) => false;
+    public override bool ShowGameModeIntroCutscene => true;
+
+    public override IEnumerator IntroCutscene(IntroCutscene __instance)
+    {
+        SoundManager.Instance.PlaySound(__instance.IntroStinger, false, 1f, null);
+        ShipStatus.Instance.BreakEmergencyButton();
+        Logger.GlobalInstance.Info("IntroCutscene :: CoBegin() :: Game Mode: Hide and Seek (MiraAPI)", null);
+        __instance.LogPlayerRoleData();
+        __instance.HideAndSeekPanels.SetActive(true);
+        if (PlayerControl.LocalPlayer.Data.Role.IsImpostor)
+        {
+            __instance.CrewmateRules.SetActive(false);
+            __instance.ImpostorRules.SetActive(true);
+        }
+        else
+        {
+            __instance.CrewmateRules.SetActive(true);
+            __instance.ImpostorRules.SetActive(false);
+        }
+
+        __instance.ImpostorName.gameObject.SetActive(true);
+        __instance.ImpostorTitle.gameObject.SetActive(true);
+        __instance.BackgroundBar.enabled = false;
+        __instance.TeamTitle.gameObject.SetActive(false);
+        var impostor = PlayerControl.AllPlayerControls.ToArray().FirstOrDefault(x => x.Data.Role.IsImpostor);
+        if (impostor == null)
+        {
+            Logger.GlobalInstance.Error("IntroCutscene :: CoBegin() :: impostor is NULL", null);
+        }
+
+        GameManager.Instance.SetSpecialCosmetics(impostor);
+        if (impostor != null)
+        {
+            __instance.ImpostorName.text = impostor.Data.PlayerName;
+        }
+        else
+        {
+            __instance.ImpostorName.text = "???";
+        }
+
+        yield return new WaitForSecondsRealtime(0.1f);
+        if (impostor != null)
+        {
+            __instance.ImpostorTitle.text = impostor.Data.Role.GetRoleName();
+        }
+        PoolablePlayer playerSlot = null;
+        if (impostor != null)
+        {
+            playerSlot = __instance.CreatePlayer(1, 1, impostor.Data, false);
+            playerSlot.SetBodyType(PlayerBodyTypes.Normal);
+            playerSlot.SetFlipX(false);
+            playerSlot.transform.localPosition = __instance.impostorPos;
+            playerSlot.transform.localScale = Vector3.one * __instance.impostorScale;
+        }
+
+        yield return ShipStatus.Instance.CosmeticsCache.PopulateFromPlayers();
+        yield return new WaitForSecondsRealtime(6f);
+        if (playerSlot != null)
+        {
+            playerSlot.gameObject.SetActive(false);
+        }
+
+        __instance.HideAndSeekPanels.SetActive(false);
+        __instance.CrewmateRules.SetActive(false);
+        __instance.ImpostorRules.SetActive(false);
+        /*LogicOptionsHnS logicOptionsHnS = GameManager.Instance.LogicOptions as LogicOptionsHnS;
+        LogicHnSMusic logicHnSMusic = GameManager.Instance.GetLogicComponent<LogicHnSMusic>() as LogicHnSMusic;
+        if (logicHnSMusic != null)
+        {
+            logicHnSMusic.StartMusicWithIntro();
+        }*/
+        var hideTimer = 10f;
+
+        if (PlayerControl.LocalPlayer.Data.Role.IsImpostor)
+        {
+            __instance.HideAndSeekTimerText.gameObject.SetActive(true);
+            PoolablePlayer poolablePlayer;
+            AnimationClip anim;
+            if (AprilFoolsMode.ShouldHorseAround())
+            {
+                poolablePlayer = __instance.HorseWrangleVisualSuit;
+                poolablePlayer.gameObject.SetActive(true);
+                poolablePlayer.SetBodyType(PlayerBodyTypes.Seeker);
+                anim = __instance.HnSSeekerSpawnHorseAnim;
+                __instance.HorseWrangleVisualPlayer.SetBodyType(PlayerBodyTypes.Normal);
+                __instance.HorseWrangleVisualPlayer.UpdateFromPlayerData(
+                    PlayerControl.LocalPlayer.Data,
+                    PlayerControl.LocalPlayer.CurrentOutfitType,
+                    PlayerMaterial.MaskType.None,
+                    false,
+                    null,
+                    false);
+            }
+            else if (AprilFoolsMode.ShouldLongAround())
+            {
+                poolablePlayer = __instance.HideAndSeekPlayerVisual;
+                poolablePlayer.gameObject.SetActive(true);
+                poolablePlayer.SetBodyType(PlayerBodyTypes.LongSeeker);
+                anim = __instance.HnSSeekerSpawnLongAnim;
+            }
+            else
+            {
+                poolablePlayer = __instance.HideAndSeekPlayerVisual;
+                poolablePlayer.gameObject.SetActive(true);
+                poolablePlayer.SetBodyType(PlayerBodyTypes.Seeker);
+                anim = __instance.HnSSeekerSpawnAnim;
+            }
+
+            poolablePlayer.SetBodyCosmeticsVisible(false);
+            poolablePlayer.UpdateFromPlayerData(
+                PlayerControl.LocalPlayer.Data,
+                PlayerControl.LocalPlayer.CurrentOutfitType,
+                PlayerMaterial.MaskType.None,
+                false,
+                null,
+                false);
+            SpriteAnim component = poolablePlayer.GetComponent<SpriteAnim>();
+            poolablePlayer.gameObject.SetActive(true);
+            poolablePlayer.ToggleName(false);
+            component.Play(anim, 1f);
+            while (hideTimer > 0f)
+            {
+                __instance.HideAndSeekTimerText.text = Mathf.RoundToInt(hideTimer).ToString();
+                hideTimer -= Time.deltaTime;
+                yield return null;
+            }
+        }
+        else
+        {
+            ShipStatus.Instance.HideCountdown = hideTimer;
+            if (AprilFoolsMode.ShouldHorseAround())
+            {
+                if (impostor != null)
+                {
+                    impostor.AnimateCustom(__instance.HnSSeekerSpawnHorseInGameAnim);
+                }
+            }
+            else if (AprilFoolsMode.ShouldLongAround())
+            {
+                if (impostor != null)
+                {
+                    impostor.AnimateCustom(__instance.HnSSeekerSpawnLongInGameAnim);
+                }
+            }
+            else if (impostor != null)
+            {
+                impostor.AnimateCustom(__instance.HnSSeekerSpawnAnim);
+                impostor.cosmetics.SetBodyCosmeticsVisible(false);
+            }
+        }
+        ShipStatus.Instance.StartSFX();
+        UnityEngine.Object.Destroy(__instance.gameObject);
+    }
 }
