4bdddc6f8a6507d5dcb508d42b58b3e3118eba5a
Atony <75191600+AtonyGit@users.noreply.github.com>
2026-08-29 19:39:21 -0500

    Fix IdPart shenanigans

--- MiraAPI/Roles/ICustomRole.cs
+++ MiraAPI/Roles/ICustomRole.cs
@@ -22,7 +22,7 @@ public interface ICustomRole : IOptionable
     /// <summary>
     /// Gets the id part used to build the role's translation keys. It is recommended to call it ModGuid.Role.Faction
     /// </summary>
-    string IdPrefix => MiraLocaleManager.BuildTranslationId(GetType().Namespace!, "Role", Team.ToString());
+    string IdPrefix => GetType().Namespace!;
 
     /// <summary>
     /// Gets the name of the role.
